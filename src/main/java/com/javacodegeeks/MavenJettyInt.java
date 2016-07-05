f5180cc65b5934c4a21da8d0dbeac427fd185230
anaMirela <ana.mirela.irimia@gmail.com>
2016-07-05 21:44:59 +0300

    yeeee

--- src/main/java/com/javacodegeeks/MavenJettyInt.java
+++ src/main/java/com/javacodegeeks/MavenJettyInt.java
@@ -19,6 +19,6 @@ public class MavenJettyInt extends HttpServlet{
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		response.setStatus(HttpServletResponse.SC_OK);
-		response.getWriter().println("<h1>Hello Servlet from my app</h1>");
+		response.getWriter().println("<h1>Hello Mada</h1>");
 	}
 }
